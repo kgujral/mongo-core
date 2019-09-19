6a741b2b76eaf2616c2482b52ae876355f3a7bd6
Karan Gujral <karan@sixsprints.com>
2019-09-19 17:12:12 +0530

    1.3.0

--- src/main/java/com/sixsprints/core/utils/DateUtil.java
+++ src/main/java/com/sixsprints/core/utils/DateUtil.java
@@ -9,10 +9,10 @@ public class DateUtil {
 
   public static final DateTimeZone JAPAN_TIME_ZONE = DateTimeZone.forID("+09:00");
 
-  private static final String DEFAULT_DATE_TIME_PATTERN = "yyyy/MM/dd HH:mm:ss";
+  private static final String DEFAULT_DATE_PATTERN = "yyyy/MM/dd";
 
   public static String dateToString(Date date) {
-    return initDateFromDate(date).toString(DEFAULT_DATE_TIME_PATTERN);
+    return initDateFromDate(date).toString(DEFAULT_DATE_PATTERN);
   }
 
   private static DateTime initDateFromDate(Date date) {
